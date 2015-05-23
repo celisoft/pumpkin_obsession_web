330fd39d5fc5912f084c10de2cb047729b06d1c5
Céline Libéral <celine.liberal@mail.com>
2015-05-23 18:40:03 +0200

    Level implementation / Difficulty OK

--- js/play.js
+++ js/play.js
@@ -8,22 +8,32 @@ var life_cpt;
 var life_cpt_display;
 var pumpkin_cpt;
 var pumpkin_cpt_display;
+var current_level;
+var notif_zone;
 
+//Called on state init.
 function play_create()
 {
+	//Init common vars
 	life_cpt = 5;
 	pumpkin_cpt = 0;
-	
+	current_level = 0;
+
 	//Load BG
 	this.game.add.image(0, 0, 'background');
 
 	//Load the pumpkin counter
-	this.game.add.image(this.game.world.width-96, 16, 'pumpkin');
-	pumpkin_cpt_display = this.game.add.text(this.game.world.width-48, 16, 'x'+pumpkin_cpt, {fontSize: '28px', fill: '#FFFFFF'});
+	this.game.add.image(this.game.world.width-128, 16, 'pumpkin');
+	pumpkin_cpt_display = this.game.add.text(this.game.world.width-78, 16, 'x'+pumpkin_cpt, {fontSize: '28px', fill: '#FFFFFF'});
 
 	//Load the life counter
-	this.game.add.image(this.game.world.width-96, 64, 'rip');
-	life_cpt_display = this.game.add.text(this.game.world.width-48, 64, 'x'+life_cpt, {fontSize: '28px', fill: '#FFFFFF'});
+	this.game.add.image(this.game.world.width-128, 64, 'rip');
+	life_cpt_display = this.game.add.text(this.game.world.width-78, 64, 'x'+life_cpt, {fontSize: '28px', fill: '#FFFFFF'});
+
+	//Notification zone initalization (on level switch)
+	notif_zone = this.game.add.text(this.game.world.width/2, this.game.world.height/2, 'Level '+current_level, {fontSize: '32px', fill: '#FFFFFF'});
+	notif_zone.anchor.setTo(0.5, 0.5);
+	this.game.time.events.add(Phaser.Timer.SECOND, reset_notif_zone, this);
 
 	//Enable physic
 	this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -46,6 +56,7 @@ function play_create()
 	player = game.add.sprite(game.world.centerX, game.world.height - 180, 'playersheet');
 	player.animations.add('walk_left', [6, 7, 8, 7], 10, true);
 	player.animations.add('walk_right', [0, 2, 1, 2], 10, true);
+	player.animations.add('blink', [9, 3], 2, true);
 	player.frame = 3;
 	this.game.physics.arcade.enable(player);
 	player.body.gravity.y = 400;
@@ -54,13 +65,16 @@ function play_create()
 	key_cursors = this.game.input.keyboard.createCursorKeys();
 }
 
+//Called to generate a new pumpkin with a random gravity
+//The gravity of generated pumpkins increase with the level.
 function generate_pumpkin()
 {
 	var pumpkinX = game.rnd.integerInRange(1, 30);
 	var pumpkin = pumpkins.create(pumpkinX*32, 0, 'pumpkin');
-	pumpkin.body.gravity.y = game.rnd.integerInRange(100, 300);
+	pumpkin.body.gravity.y = game.rnd.integerInRange(100, 300) + current_level*50;
 }
 
+//Called when a pumpkin collides with the ground
 function miss_pumpkin(pumpkin, ground)
 {
 	pumpkin.kill();
@@ -76,9 +90,12 @@ function miss_pumpkin(pumpkin, ground)
 	}
 }
 
+//Called when player collides with a pumpkin
 function get_pumpkin(player, pumpkin)
 {
 	pumpkin.kill();
+
+	player.animations.play('blink');
 
 	if(!snd_get_pumpkin)
 	{
@@ -88,13 +105,32 @@ function get_pumpkin(player, pumpkin)
 
 	pumpkin_cpt++;
 	pumpkin_cpt_display.text = 'x'+pumpkin_cpt;
+
+	if(pumpkin_cpt % 10 == 0)
+	{
+		if(life_cpt < 5)
+		{
+			life_cpt++;
+			life_cpt_display.text = 'x'+life_cpt;
+		}
+
+		current_level++;
+		notif_zone.text = "Level " + current_level;
+		this.game.time.events.add(Phaser.Timer.SECOND, reset_notif_zone, this);
+	}
 }
 
+//Used to reset the text area after diplaying someting
+function reset_notif_zone()
+{
+	notif_zone.text = "";	
+}
+
+//Call on each refresh
 function play_update()
 {
 	this.game.physics.arcade.collide(player, ground);
 	this.game.physics.arcade.overlap(pumpkins, ground, miss_pumpkin, null, this);
-	this.game.physics.arcade.overlap(player, pumpkins, get_pumpkin, null, this);
 
 	player.body.velocity.x = 0;
 	if(key_cursors.left.isDown)
@@ -118,4 +154,6 @@ function play_update()
 		player.frame = 5;
 		player.body.velocity.y = -350;
 	}
+
+	this.game.physics.arcade.overlap(player, pumpkins, get_pumpkin, null, this);
 }
